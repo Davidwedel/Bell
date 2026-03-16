ca64962de4a2c135c48dae8ee095e39c51a65e93
David Wedel <davidwedel06@gmail.com>
2026-03-16 17:52:39 -0500

    add wifi upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ESPAsyncWebServer.h>
 #include <Preferences.h>
 #include <ArduinoJson.h>
+#include <ArduinoOTA.h>
 #include <time.h>
 
 // WiFi credentials - CHANGE THESE
@@ -106,6 +107,35 @@ void setup() {
   Serial.println("\nWiFi connected with static IP!");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Configure OTA updates
+  ArduinoOTA.setHostname("bell-controller");
+  ArduinoOTA.setPassword("bell");  // Change this to a secure password
+
+  ArduinoOTA.onStart([]() {
+    String type = (ArduinoOTA.getCommand() == U_FLASH) ? "sketch" : "filesystem";
+    Serial.println("OTA: Start updating " + type);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA: Update complete");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
 
   // Load preferences
   preferences.begin("bell", false);
@@ -504,6 +534,7 @@ void setup() {
 }
 
 void loop() {
+  ArduinoOTA.handle();
   checkSchedules();
   handleButton();
 
